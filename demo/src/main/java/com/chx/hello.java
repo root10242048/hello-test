4203f3c2c636f3297761be7278c2a136fbf9485d
hexiaoli <hexiaoli@topdraw.cn>
2023-10-10 20:57:12 +0800

    Revert "Revert "修改工程1""
    This reverts commit c2abbf3a089d95f2488e0e5079a5f219d9d59abc.

--- demo/src/main/java/com/chx/hello.java
+++ demo/src/main/java/com/chx/hello.java
@@ -4,5 +4,10 @@ public class hello {
     public static void main(String[] args) {
 
         System.out.println("hello world");
+        System.out.println("hello world");
+
+        System.out.println("laji");
+
+
     }
 }
